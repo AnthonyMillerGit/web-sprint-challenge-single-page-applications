88606d2cb47bf06557efbc1e5d2ce55c9f521f9c
ferengitooth <ehitsamill@gmail.com>
2021-05-21 14:19:08 -0700

    added form skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Title from './Components/Title'
+import Form from './Components/Form'
 
 const App = () => {
   return (
     <>
       <Title/>
-      <p>You can remove this code and create your own header</p>
+      <Form />
     </>
   );
 };
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,9 +6,108 @@ function Form () {
         <div>
             <h3>Build Your Own Pizza!</h3>
             <form>
-                <input
+                <label>Choice Of Size
+                    <select name='size'>
+                    <option value=''>-- Select a size --</option>
+                    <option value='Small'>Small</option>
+                    <option value='Medium'>Medium</option>
+                    <option value='Large'>Large</option>
+                    <option value='XLarge'>X-Large</option>
+                    </select>
+                </label>
+            <h3>Choose Type Of Sauce</h3>
+                <label>Original Red
+                    <input 
+                    type='radio'
+                    name='civil'
+                    value='Original Red'
+                    // onChange={onChange}
+                    // checked={values.civil === 'single'}
+                    />
+                </label>
+        
+                <label>Garlic Ranch
+                    <input
+                    type='radio'
+                    name='civil'
+                    value='Garlic Ranch'
+                    // onChange={onChange}
+                    // checked={values.civil === 'married'}
+                    />
+                </label>
+                <label>BBQ Sauce
+                    <input 
+                    type='radio'
+                    name='civil'
+                    value='BBQ Sauce'
+                    // onChange={onChange}
+                    // checked={values.civil === 'single'}
+                    />
+                </label>
+        
+                <label>Spinach Alfredo
+                    <input
+                    type='radio'
+                    name='civil'
+                    value='Spinach Alfredo'
+                    // onChange={onChange}
+                    // checked={values.civil === 'married'}
+                    />
+                </label>
+            <h3>Add Toppings</h3>
+                <label>Cheese
+                    <input 
+                    type='checkbox'
+                    name='Cheese'
+                    // checked={values.hiking}
+                    // onChange={onChange}
+                    />
+                </label>
+        
+                <label>Pepperoni
+                    <input 
+                    type='checkbox'
+                    name='Pepperoni'
+                    // checked={values.reading}
+                    // onChange={onChange}
+                    />
+                </label>
+        
+                <label>Anchovies
+                    <input 
+                    type='checkbox'
+                    name='Anchovies'
+                    // checked={values.coding}
+                    // onChange={onChange}
+                    />
+                </label>
+                <label>Sausage
+                    <input 
+                    type='checkbox'
+                    name='Sausage'
+                    // checked={values.hiking}
+                    // onChange={onChange}
+                    />
+                </label>
+        
+                <label>Mushrooms
+                    <input 
+                    type='checkbox'
+                    name='Mushrooms'
+                    // checked={values.coding}
+                    // onChange={onChange}
+                    />
+                </label>
+                <label>Peppers
+                    <input 
+                    type='checkbox'
+                    name='Peppers'
+                    // checked={values.coding}
+                    // onChange={onChange}
+                    />
+                </label>
                     
-                 />
+
             </form>
         </div>
     )
