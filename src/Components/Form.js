a8b7d25dfb1f4a7c555fe5084d400008f1f4da79
ferengitooth <ehitsamill@gmail.com>
2021-05-21 13:19:19 -0700

    i think i have all modules downloaded and app skeleton

---
+++ src/Components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Form () {
+    
+    return (
+        <div>
+            <h3>Build Your Own Pizza!</h3>
+            <form>
+                <input
+                    
+                 />
+            </form>
+        </div>
+    )
+}
+
+export default Form
